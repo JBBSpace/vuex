7ec4f7622fdac3e3dd1770f28bdb9e4439fdea86
姬兵兵 <1204680869@qq.com>
2018-06-06 09:01:58 +0800

    测试上传

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: 姬兵兵 
+ * @Date: 2018-06-06 09:00:30 
+ * @Last Modified by:   姬兵兵 
+ * @Last Modified time: 2018-06-06 09:00:30 
+ */
 import { Store, install } from './store'
 import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
